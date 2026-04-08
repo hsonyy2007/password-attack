f31b032f455743764e75ad6ea0f1a0efa003dc6a
Hussein Mehdi Diaa Abbas <07742951428hu@gmail.com>
2026-04-09 00:08:32 +0300

    Update HSONY2.py

--- HSONY2.py
+++ HSONY2.py
@@ -1,6 +1,6 @@
 import itertools
 
-data = ["Ali", "BMW2000", "Ahmed", "1980", "Alaa", "CR7", "Managemet"]
+data = ["Ali", "BMW2000", "Ahmed", "1980", "Alaa","aaa" , "CR7", "Managemet"]
 
 for i in [1,2, 3]:
     for j in itertools.permutations(data, i):
